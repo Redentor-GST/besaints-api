c76a5b1239c5bd39b687e241e3244a9fc947ff6b
jramost <jramos@seenka.com>
2023-02-04 16:07:52 -0300

    tests config

--- src/services/tests/phraseService.spec.ts
+++ src/services/tests/phraseService.spec.ts
@@ -1,15 +1,26 @@
-import PhraseService from '../phrasesService'
+import connect from "../../db/db"
+import PhraseService from "../phrasesService"
 
 describe('PhraseService Tests', () => {
   it('should return a valid random phrase', () => {
-    const phraseService = new PhraseService()
-    const phrase = phraseService.getRandomPhrase()
-    expect(phrase).toHaveProperty('text')
-    expect(phrase).toHaveProperty('author')
+    connect().then(_ => {
+      const phraseService = new PhraseService()
+      const phrase = phraseService.getRandomPhrase()
+      expect(phrase).toHaveProperty('text')
+      expect(phrase).toHaveProperty('author')
+    })
   })
 
-  it('should initialize db correctly', () => {
-    const phraseService = new PhraseService()
-    expect(phraseService.db).toBeDefined()
+  it('should return a valid list of phrases', async () => {
+    connect().then(_ => {
+      const phraseService = new PhraseService()
+      phraseService.getPhrases().then(phrases => {
+        expect(phrases).toBeInstanceOf(Array)
+        for (const phrase of phrases) {
+          expect(phrase).toHaveProperty('text')
+          expect(phrase).toHaveProperty('author')
+        }
+      })
+    })
   })
 })
