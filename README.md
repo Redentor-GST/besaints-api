fbf75e3000b03460727d512113b6aa7c04b47dad
jramost <jramos@seenka.com>
2023-02-05 16:39:59 -0300

    halfway

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 </p>
 
 [![codecov](https://codecov.io/gh/jramosss/besaints-api/branch/master/graph/badge.svg?token=5XGVU6XQK5)](https://codecov.io/gh/jramosss/besaints-api)
+[![Netlify Status](https://api.netlify.com/api/v1/badges/c8eecfd2-c0d2-441a-be63-ac5c983cf178/deploy-status)](https://app.netlify.com/sites/besaints-api/deploys)
 
 A minimalistic API for saints phrases
 
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import express from 'express'
 import phrasesRouter from './routes/phrasesRouter'
 import connect from './db/db'
+import serverless from 'serverless-http'
+
 
 const app = express()
 app.use(express.json())
 app.set('json spaces', 2)
+module.exports.handler = serverless(app)
 
 const PORT = 3000
 
@@ -14,7 +17,7 @@ app.get('/', (req, res) => {
 
 connect().then(db => {
   app.locals.db = db
-  app.use('/api/phrases/', phrasesRouter)
+  app.use('/.netlify/functions/phrases', phrasesRouter)
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
